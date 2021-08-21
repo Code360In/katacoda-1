4e6f2c343e5c740a19e13dfb7fb32d53e9411b3d
auk003150 <69250701+auk003150@users.noreply.github.com>
2021-08-21 19:04:57 +0800

    Update background.sh

--- linuxTutorial/lesson5/background.sh
+++ linuxTutorial/lesson5/background.sh
@@ -1,4 +1,5 @@
 mkdir helloworld
 cd helloworld
 
-echo "Hello this is your first text file." > index.txt
+echo "Hello this is your first text file." > text1.txt
+echo "Hello this is your second text file." > text2.txt

cc434f7fe6fa560653c93f840801eae7e7e4613d
auk003150 <69250701+auk003150@users.noreply.github.com>
2021-08-21 21:03:11 +0800

    Update background.sh

--- linuxTutorial/lesson5/background.sh
+++ linuxTutorial/lesson5/background.sh
@@ -5,5 +5,4 @@ echo "Hello this is your first text file." > text1.txt
 echo "Hello this is your second text file." > text2.txt
 
 cat text1.txt text1.txt > combined.txt
-cat text1.txt text2.txt > sort.txt
-cat text1.txt text1.txt text2.txt > exercise.txt
+cat text1.txt text1.txt text2.txt > sort.txt

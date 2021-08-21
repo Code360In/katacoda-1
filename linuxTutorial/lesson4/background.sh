991d4b30b5853f404a22fdf35d5b8bc5bc5534d5
auk003150 <69250701+auk003150@users.noreply.github.com>
2021-08-21 16:36:12 +0800

    Update background.sh

--- linuxTutorial/lesson4/background.sh
+++ linuxTutorial/lesson4/background.sh
@@ -2,4 +2,6 @@ mkdir /helloworld
 cd /helloworld
 
 mkdir dir1 dir2 dir3
-echo "This is a text file." > text1.txt
+echo "This is the index file." > index.txt
+echo "This is the first text file." > text1.txt
+echo "This is the second text file." > text2.txt

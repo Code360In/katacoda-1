04c2cb1ca3c33ab918a2521f3126ae207b17eb3a
auk003150 <69250701+auk003150@users.noreply.github.com>
2021-08-21 20:43:00 +0800

    Update background.sh

--- linuxTutorial/lesson5/background.sh
+++ linuxTutorial/lesson5/background.sh
@@ -2,6 +2,4 @@ mkdir helloworld
 cd helloworld
 
 echo "Hello this is your first text file." > text1.txt
-echo "Hello this is your second text file." > text2.txt
-
 cat text1.txt text1.txt > combined.txt
